d827e671c73defa728547c421ecee0d5c3d49212
Nate Beaty <nate@clixel.com>
2019-08-21 12:47:58 -0400

    initial commit

---
+++ config/general.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see \craft\config\GeneralConfig
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // Default Week Start Day (0 = Sunday, 1 = Monday...)
+        'defaultWeekStartDay' => 1,
+
+        // Whether generated URLs should omit "index.php"
+        'omitScriptNameInUrls' => true,
+
+        // Control Panel trigger word
+        'cpTrigger' => 'admin',
+
+        // The secure key Craft will use for hashing and encrypting data
+        'securityKey' => getenv('SECURITY_KEY'),
+
+        // Whether to save the project config out to config/project.yaml
+        // (see https://docs.craftcms.com/v3/project-config.html)
+        'useProjectConfigFile' => true,
+    ],
+
+    // Dev environment settings
+    'dev' => [
+        // Fix backing up mysql on dev w/ homebrew mysql
+        'backupCommand' =>  "/usr/local/bin/mysqldump -h localhost -u root -proot --add-drop-table --comments --create-options --dump-date --no-autocommit --routines --set-charset --triggers --single-transaction --no-data --result-file=\"{file}\" {database} && /usr/local/bin/mysqldump -h localhost -u root -proot --add-drop-table --comments --create-options --dump-date --no-autocommit --routines --set-charset --triggers --no-create-info --ignore-table={database}.assetindexdata --ignore-table={database}.assettransformindex --ignore-table={database}.cache --ignore-table={database}.sessions --ignore-table={database}.templatecaches --ignore-table={database}.templatecachecriteria --ignore-table={database}.templatecacheelements {database} >> \"{file}\"",
+        'restoreCommand' => "/usr/local/bin/mysql -h localhost -u root -proot {database} < \"{file}\"",
+        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
+        'devMode' => true,
+    ],
+
+    // Staging environment settings
+    'staging' => [
+        // Set this to `false` to prevent administrative changes from being made on staging
+        'allowAdminChanges' => false,
+    ],
+
+    // Production environment settings
+    'production' => [
+        // Set this to `false` to prevent administrative changes from being made on production
+        'allowAdminChanges' => false,
+    ],
+];
