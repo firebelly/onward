2770170b0b9eb4649fd108c248331aff2ceaab5a
Nate Beaty <nate@clixel.com>
2019-09-19 17:25:24 -0500

    add js handling for site navigation child elements

--- assets/scripts/routes/common.js
+++ assets/scripts/routes/common.js
@@ -1,11 +1,37 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
-    console.log('common-init');
+
+    // Handle clicks on nav a elements
+    $('.site-nav a').on('click', function(e) {
+      var $this = $(this);
+      var $li = $this.parents('li:first');
+      var href = $this.attr('href');
+      var hrefSplit = href.split('#');
+      console.log($li);
+
+      // Mobile support for ul.children, slide in/out child options
+      if ($li.hasClass('dropdown')) {
+        e.preventDefault();
+        $('.site-nav li').not($li).removeClass('open');
+        $li.toggleClass('open');
+        return;
+      }
+
+      // Close mobile nav on click (if item just scrolls page down)
+      $('body').removeClass('mobile-nav-open');
+
+      // Just scroll down to section if anchor link on same page
+      if (hrefSplit.length && page_at == hrefSplit[0] && hrefSplit[0] != '#') {
+        e.preventDefault();
+        e.target.blur();
+        _scrollBody('#' + hrefSplit[1], 500);
+      }
+    });
+
 
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
-    console.log('common-finalize');
   },
 };
--- assets/scripts/routes/home.js
+++ assets/scripts/routes/home.js
@@ -1,10 +1,8 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
-    console.log('home-init');
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
-	  console.log('home-finalize');
   },
 };
