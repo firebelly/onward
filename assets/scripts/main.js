65cce90b3d93857923ecffe2725d75ae71cef67e
Nate Beaty <nate@clixel.com>
2019-10-16 18:47:22 -0400

    add ourTeam js to main init

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,15 +7,17 @@ import stickyHeaders from './util/stickyHeaders';
 import stickyNav from './util/stickyNav';
 import common from './routes/common';
 import pageHome from './routes/home';
+import childOurTeam from './routes/our-team';
 
 /** Populate Router instance with DOM routes */
 const routes = new Router({
   common,
   pageHome,
+  childOurTeam,
 });
 
 // Init sticky headers
-stickyHeaders.init($('.sticky-header'), $(window), 70);
+stickyHeaders.init($('.sticky-header'), $(window), 20);
 
 // Init sticky nav
 stickyNav.init();
