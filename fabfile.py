ebcaef8bf99997b21ccaa1ee76aa370ff8b889fa
Nate Beaty <nate@clixel.com>
2019-11-19 15:02:39 -0500

    update fabfile for opalstack

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,27 @@
 from fabric.api import *
 import os
 
-env.hosts = ['onward.firebelly.co']
-env.user = 'firebelly'
+env.hosts = ['staging.thisisonward.com']
+env.user = 'caitlinmccorkle'
 env.path = '~/Firebelly/onward'
-env.remotepath = '/home/firebelly/webapps/onward_dev'
+env.remotepath = '/home/onward/apps/onward-staging'
 env.git_branch = 'master'
 env.warn_only = True
 env.remote_protocol = 'http'
 
 def production():
-  env.hosts = ['thisisonward.org']
+  env.hosts = ['thisisonward.com']
   env.user = 'onward'
-  env.remotepath = '/home/onward/webapps/onward_production'
+  env.remotepath = '/home/onward/apps/onward-production'
   env.git_branch = 'master'
   env.remote_protocol = 'https'
 
 def devsetup():
-  print "Installing composer, yarn...\n"
+  print("Installing composer, yarn...\n")
   local('composer install')
   local('yarn')
   local('cp .env-example .env')
-  print "OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets"
+  print("OK DONE! Hello? Are you still awake?\nEdit your .env file with local credentials\nRun `npx gulp watch` to run local gulp to compile & watch assets")
 
 def deploy(composer='y', assets='y'):
   update()
@@ -39,4 +39,4 @@ def update():
 
 def composer_install():
   with cd(env.remotepath):
-    run('php73 ~/bin/composer.phar install')
+    run('~/bin/composer install')
